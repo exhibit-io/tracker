8e5f9b012c8b6f67b9cfdbd58b09218ebb759c5e
fenix-hub <n.santilio97@gmail.com>
2024-08-03 15:19:04 +0000

    change port and files

--- tracker/config/tracker.go
+++ tracker/config/tracker.go
@@ -14,6 +14,6 @@ func (t TrackerConfig) GetAddr() string {
 func LoadTrackerConfig() TrackerConfig {
 	return TrackerConfig{
 		Host: getEnv("HOST", "localhost"),
-		Port: getEnv("PORT", "3000"),
+		Port: getEnv("PORT", "8080"),
 	}
 }
