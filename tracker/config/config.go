e7dfd8d09e9e5db48ede40fd6a739957576fc04f
nsantilio-devocntainer <nicolo.santilio@outlook.com>
2024-08-29 14:01:18 +0000

    fix configuration class

--- tracker/config/config.go
+++ tracker/config/config.go
@@ -1,6 +1,9 @@
 package config
 
-import "github.com/joho/godotenv"
+import (
+	"github.com/joho/godotenv"
+	"log"
+)
 
 // Config represents the main configuration struct containing all service configs.
 type Config struct {
@@ -13,7 +16,7 @@ type Config struct {
 func LoadConfig() *Config {
 	err := godotenv.Load()
 	if err != nil {
-		return nil
+		log.Default().Print("No .env file found, using default.")
 	}
 	return &Config{
 		Redis:   LoadRedisConfig(),
